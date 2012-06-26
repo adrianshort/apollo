360587a1ad58b3d623ac575d00d98417c2cc99d0
Adrian Short <adrian.short@gmail.com>
2012-06-26 18:02:30 +0100

    Added start of custom response for Layar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
 class PostsController < ApplicationController
+  include PostsHelper
+  
   def near
-    @posts = Post.near(params[:lat].to_f, params[:lng].to_f, params[:radius].to_f)
+    @posts = Post.near(params[:lat].to_f, params[:lon].to_f, params[:radius].to_f)
+
+    layar_response = {
+      :layer =>         'hyparlocal',
+      :hotspots =>      @posts.collect { |p| post_to_poi(p) },
+      :errorCode =>     0, # OK
+      :errorString =>   "OK",
+      :radius =>        params[:radius].to_f
+    }
 
     respond_to do |format|
       format.html # near.html.erb
       format.json { render json: @posts }
+      format.layar { render json: layar_response }
     end
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+  def post_to_poi(post)
+    # http://layar.com/documentation/browser/api/getpois-response/
+    {
+      :id => post._id,
+      :anchor =>  {
+        :geolocation => {
+          :lat => post.loc['lat'],
+          :lon => post.loc['lng'],
+          :alt => 0
+        }
+      },
+      :title => post.title
+    }
+  end
 end
