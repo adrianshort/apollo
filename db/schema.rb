da5e5adbafa2876972f99fcdf0e757f0480bcd74
Adrian Short <adrian.short@gmail.com>
2013-02-22 12:03:26 +0000

    Remove username from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130222112030) do
+ActiveRecord::Schema.define(:version => 20130222120002) do
 
   create_table "feeds", :force => true do |t|
     t.string   "title"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(:version => 20130222112030) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "username",         :null => false
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
